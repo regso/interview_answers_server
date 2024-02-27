36b6fc94cc9f42c32894cdbe2f8b3df4dad52e56
regso <55665811+regso@users.noreply.github.com>
2024-02-27 22:11:27 +0300

    Init.

---
+++ bin/interview_answers_server.dart
@@ -0,0 +1,5 @@
+import 'package:interview_answers_server/interview_answers_server.dart' as interview_answers_server;
+
+void main(List<String> arguments) {
+  print('Hello world: ${interview_answers_server.calculate()}!');
+}
---
+++ test/interview_answers_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:interview_answers_server/interview_answers_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
